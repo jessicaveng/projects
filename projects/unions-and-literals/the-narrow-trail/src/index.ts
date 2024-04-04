35275a8d0701a452287e17ca507d80c0de2447ca
jessicaveng <jessicaveng@outlook.com>
2024-04-04 21:44:17 +1300

    completed narrow trail

--- projects/unions-and-literals/the-narrow-trail/src/index.ts
+++ projects/unions-and-literals/the-narrow-trail/src/index.ts
@@ -1,3 +1,64 @@
+import { Waterfall } from "../../../interfaces/vacation-planning/03-landmarks/solution";
 export function runCommands() {
 	// Declare your variables and runtime logic here! ✨
+	let resupply: "food" | "water" | undefined;
+	let food: number = 5;
+	let water: number = 5;
+	let day: number = 1;
+
+	for (day; day <= 7; day += 1) {
+		const randomNum = Math.floor(Math.random() * 6) + 1;
+		let command: "water" | "food" | number;
+
+		switch (randomNum) {
+			case 1:
+				command = "food";
+				break;
+
+			case 2:
+				command = "water";
+				break;
+
+			default:
+				command = randomNum;
+				break;
+		}
+
+		if (typeof command === "number") {
+			switch (resupply) {
+				case "food":
+					food += command;
+					resupply = undefined;
+					break;
+
+				case "water":
+					water += command;
+					resupply = undefined;
+					break;
+
+				default:
+					resupply = randomNum % 2 === 0 ? "food" : "water";
+					break;
+			}
+		}
+
+		switch (command) {
+			case "food":
+				resupply = "food";
+				break;
+
+			case "water":
+				resupply = "water";
+				break;
+		}
+
+		food -= 1;
+		water -= 1;
+
+		if (food === 0 || water === 0) {
+			return false;
+		}
+	}
+
+	return true;
 }
